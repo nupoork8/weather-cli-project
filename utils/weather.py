846353a7643a5fa412674d61ba95e0bbc14d34c0
nupoork8 <nupoorktech@gmail.com>
2025-11-28 00:13:31 +0530

    Add weather.py

---
+++ utils/weather.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+def get_weather(city, api):
+    url = "http://api.openweathermap.org/data/2.5/weather"
+    api_key = ""
+    params = {
+        'q': city,
+        'api_id' : api_key,
+        'units' : 'metric'
+    }    
+    response = response.get(url, params = params , timeout = 10)
+    return response.json()
+
+def main():
+    
