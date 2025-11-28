077737d67fc69a7ceb070ad3e84b5838295b8a92
nupoork8 <nupoorktech@gmail.com>
2025-11-28 17:41:12 +0530

    Add app file

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 from utils.weather import get_weather
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
 API_KEY = "57c4d0ef3cebaf6d9b98ff3ed5a7fb40"
+
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -12,22 +12,19 @@ def home():
 
     if request.method == "POST":
         city = request.form.get("city")
+        data = get_weather(city, API_KEY)
 
-        if city:
-            data = get_weather(city, API_KEY)
+        if "main" in data:
+            weather_data = {
+                "city": city,
+                "temp": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "desc": data["weather"][0]["description"]
+            }
+        else:
+            error = "City not found! Try another city."
 
-            if data.get("cod") != 200:
-                error = data.get("message", "Something went wrong!")
-            else:
-                weather_data = {
-                    "city": city,
-                    "temp": data["main"]["temp"],
-                    "humidity": data["main"]["humidity"],
-                    "desc": data["weather"][0]["description"]
-                }
-
-    return render_template("index.html", weather=weather_data, error=error)
-
+    return render_template("index.html", weather_data=weather_data, error=error)
 
 if __name__ == "__main__":
     app.run(debug=True)
