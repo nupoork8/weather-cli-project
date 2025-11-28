e89d2f54f24c17771ec74ec09e272dd9f4ec8671
nupoork8 <nupoorktech@gmail.com>
2025-11-28 13:45:18 +0530

    Add weather file

--- utils/weather.py
+++ utils/weather.py
@@ -1,16 +1,12 @@
 import requests
-import sys
 
-def get_weather(city, api):
-    url = "http://api.openweathermap.org/data/2.5/weather"
-    api_key = ""
-    params = {
-        'q': city,
-        'api_id' : api_key,
-        'units' : 'metric'
-    }    
-    response = response.get(url, params = params , timeout = 10)
-    return response.json()
+def get_weather(city,api_key):
+    # To get weather, I need two things:
+    # 1. The API link
+    # 2. The city name + my API key
+
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
 
 def main():
-    get_weather("mumbai")
+    pass
