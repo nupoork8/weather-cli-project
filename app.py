d2d190ce3a34fbfe34aacd0e716d9e7f7df77fe9
nupoork8 <nupoorktech@gmail.com>
2025-11-28 17:32:50 +0530

    Add app file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from weather import get_weather
+from utils.weather import get_weather
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 API_KEY = "57c4d0ef3cebaf6d9b98ff3ed5a7fb40"
 
@@ -12,23 +12,22 @@ def home():
 
     if request.method == "POST":
         city = request.form.get("city")
-        
-        if not city:
-            error = "Please enter a city name."
-        else:
+
+        if city:
             data = get_weather(city, API_KEY)
 
-            if "main" not in data:
-                error = "City not found. Try again!"
+            if data.get("cod") != 200:
+                error = data.get("message", "Something went wrong!")
             else:
                 weather_data = {
                     "city": city,
                     "temp": data["main"]["temp"],
                     "humidity": data["main"]["humidity"],
-                    "desc": data["weather"][0]["description"].title()
+                    "desc": data["weather"][0]["description"]
                 }
 
     return render_template("index.html", weather=weather_data, error=error)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
